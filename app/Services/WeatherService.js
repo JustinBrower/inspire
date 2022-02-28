cdc76626aaa604f35a7647ca9511b5549b83b25b
Justin Brower <justin.lee.brower@gmail.com>
2022-02-28 00:23:14 -0700

    comitt

--- app/Services/WeatherService.js
+++ app/Services/WeatherService.js
@@ -3,6 +3,7 @@ import { sandboxApiW } from "./AxiosService.js";
 
 
 
+let orTemp = true
 
 
 class WeatherService {
@@ -12,8 +13,29 @@ class WeatherService {
         console.log("Temperature is...", res.data.main.temp);
         ProxyState.weather = res.data
     }
+
+    async switchWeather() {
+        (orTemp = !orTemp)
+        console.log(orTemp);
+    }
+
+    async drawWeather() {
+        let temperature = ProxyState.weather.main.temp
+        console.log(temperature);
+        let f = Math.round(((9 / 5) * (temperature - 273)) + 32)
+        let c = Math.round(temperature - 273)
+        console.log("f is...", f);
+        console.log("c is...", c);
+
+        let template = ''
+
+        if (orTemp == true) {
+            template = `<div onclick="app.weatherController.switchWeather()">${f} Degrees Farenheit</div>`
+        } else {
+            template = `<div onclick="app.weatherController.switchWeather()">${c} Degrees Celsius</div>`
+        }
+        document.getElementById('temp').innerHTML = template
+    }
 }
-
-
 
 export const weatherService = new WeatherService()
