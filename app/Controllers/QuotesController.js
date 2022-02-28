bbc19d6934e8c8418c49a39b1c57c87defeb7414
Justin Brower <justin.lee.brower@gmail.com>
2022-02-28 09:16:13 -0700

    comitt

--- app/Controllers/QuotesController.js
+++ app/Controllers/QuotesController.js
@@ -12,10 +12,10 @@ async function _getQuotes() {
 
 async function _drawQuotes() {
     let template = `<div class="justify-content-center align-items-center">
-        <div class="text-light">${ProxyState.quotes.content}</div>
+        <div class="text-light"><h3>${ProxyState.quotes.content}</h3></div>
         </div>
         <div id="author" class="d-flex justify-content-center align-items-center">
-        <div class="text-light">${ProxyState.quotes.author}</div>
+        <div>${ProxyState.quotes.author}</div>
         </div>`
     document.getElementById('quotes-message').innerHTML = template
 }
