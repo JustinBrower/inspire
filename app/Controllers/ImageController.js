a4421d070735216e8018d943ea5568f7cbb24c54
Justin Brower <justin.lee.brower@gmail.com>
2022-02-27 21:05:59 -0700

    comitt

--- app/Controllers/ImageController.js
+++ app/Controllers/ImageController.js
@@ -1,10 +1,34 @@
 import { ProxyState } from "../AppState.js";
 import { imageService } from "../Services/ImageService.js"
 
+/// DRAWING THE CLOCK HERE HOPE THATS CHILL
+
+async function _findTime() {
+    console.log("setting time interval...");
+    setInterval(_drawClock, 1000)
+}
+
+async function _drawClock() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let late = "AM"
+    if (hours > 12) {
+        hours = hours - 12
+        late = "PM"
+    }
+    let template = `<div>${hours}:${minutes}:${seconds}${late}</div>`
+    console.log(template);
+    document.getElementById('clock-widget').innerHTML = template
+}
+
+/// END CLOCK TEMPLATE
+
+
 
 
 async function _drawImage() {
-    // THIS GIVES AN ERROR FOR TEMPLATE
     let template = `<img src="${ProxyState.image.largeImgUrl}" alt="background image"></img>`
     document.getElementById('image').innerHTML = template
     console.log("The Controller logs template as...", template);
@@ -22,6 +46,7 @@ export class ImageController {
     constructor() {
         console.log("ImageController Loaded...");
         ProxyState.on("image", _drawImage)
+        _findTime()
         _getImage()
     }
 }
